ec5975aa7fc8e39d7dd58d714c7be64bcd05a7c9
admin <1762438122@qq.com>
2020-04-21 16:15:00 +0800

    2020/4/21

---
+++ app/src/main/java/cn/edu/sdwu/android/class02/sn170507180111/MainActivity.java
@@ -0,0 +1,12 @@
+package cn.edu.sdwu.android.class02.sn170507180111;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
